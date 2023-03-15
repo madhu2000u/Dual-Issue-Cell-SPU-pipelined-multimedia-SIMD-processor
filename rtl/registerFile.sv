837349b858c1e137e41521700dedd16f9855ce75
Madhu Sudhanan <madhu2000u@gmail.com>
2023-03-15 03:23:34 -0400

    Compile errors fixed

--- rtl/registerFile.sv
+++ rtl/registerFile.sv
@@ -27,14 +27,15 @@ module registerFile (
     regWr_en_odd
 );             
 
-    input clk, reset;
-    input [0:QUADWORD-1]            rt_wt_even, rt_wt_odd;
-    output logic [0:QUADWORD-1]     ra_rd_even, rb_rd_even, rc_rd_even, ra_rd_odd, rb_rd_odd;
-    input [0:ADDR_WIDTH-1]          addr_ra_rd_even, addr_rb_rd_even, addr_rc_rd_even, addr_ra_rd_odd, addr_rb_rd_odd;
-    input [0:ADDR_WIDTH-1]          addr_rt_wt_even, addr_rt_wt_odd;
-    input                           regWr_en_even, regWr_en_odd;
+    input                               clk, reset;
+    input [0 : QUADWORD - 1]            rt_wt_even, rt_wt_odd;
+    input [0 : REG_ADDR_WIDTH - 1]      addr_ra_rd_even, addr_rb_rd_even, addr_rc_rd_even, addr_ra_rd_odd, addr_rb_rd_odd;
+    input [0 : REG_ADDR_WIDTH - 1]      addr_rt_wt_even, addr_rt_wt_odd;
+    input                               regWr_en_even, regWr_en_odd;
+
+    output logic [0:QUADWORD-1]         ra_rd_even, rb_rd_even, rc_rd_even, ra_rd_odd, rb_rd_odd;
     
-    logic [0:QUADWORD-1] registerFile [0:REG_COUNT-1];
+    logic [0:QUADWORD-1]                registerFile [0:REG_COUNT-1];
 
     assign ra_rd_even = registerFile[addr_ra_rd_even];
     assign rb_rd_even = registerFile[addr_rb_rd_even];
